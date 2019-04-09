91c5ce9145523f6eedf55be60201310d6dadc564
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2019-04-09 13:11:41 +0200

    GPS providers

--- app/src/main/java/hu/szte/kotprog/MainActivity.java
+++ app/src/main/java/hu/szte/kotprog/MainActivity.java
@@ -1,20 +1,67 @@
 package hu.szte.kotprog;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int START_TREASURE = 1;
+    public static final int PERMISSION = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, this.PERMISSION);
+        }
+
     }
 
     public void start(View view) {
         Intent intent = new Intent(this, TreasureHunterActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, START_TREASURE);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case (START_TREASURE):
+                if (resultCode == RESULT_OK) {
+                    Toast.makeText(MainActivity.this, data.getStringExtra(String.valueOf(R.string.EXTRA_REPLY)), Toast.LENGTH_LONG).show();
+                }
+                break;
+            case (PERMISSION): {
+                Toast.makeText(MainActivity.this, String.valueOf(resultCode), Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case (PERMISSION): {
+                if (grantResults[0] == -1) {
+                    Toast.makeText(MainActivity.this, R.string.NO_PERMISSION, Toast.LENGTH_LONG).show();
+                    this.finish();
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/hu/szte/kotprog/TreasureHunterActivity.java
+++ app/src/main/java/hu/szte/kotprog/TreasureHunterActivity.java
@@ -1,35 +1,87 @@
 package hu.szte.kotprog;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class TreasureHunterActivity extends AppCompatActivity {
 
 
+    private LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.locationManager =  (LocationManager) getSystemService(LOCATION_SERVICE);
+        this.locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER,
+                2000,
+                10, this.locationListener);
+
         setContentView(R.layout.activity_treasure_hunter);
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        boolean enabled = this.locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-// Define a listener that responds to location updates
-        LocationListener locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                // Called when a new location is found by the network location provider.
-                Toast.makeText(TreasureHunterActivity.this, location.toString(), Toast.LENGTH_SHORT).show();
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-            public void onProviderEnabled(String provider) {}
-
-            public void onProviderDisabled(String provider) {}
-        };
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(intent, 1);
+        }
 
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (this.locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
+
+        }
+        else{
+
+        }
+
+        String EXTRA_REPLY = String.valueOf(R.string.EXTRA_REPLY);
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, getResources().getString(R.string.NOGPS));
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            Log.i("test",String.valueOf(location.getLatitude()));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+
 }
